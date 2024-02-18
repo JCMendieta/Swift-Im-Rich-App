89da559ccaf46510fe78b0f31700789043c4b051
camilo <juank.yoh.117@gmail.com>
2024-02-17 19:07:38 -0500

    Initial Commit

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Juan Camilo Mendieta Hernández on 17/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
